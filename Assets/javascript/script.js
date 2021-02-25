1b39378ea9c3c0fe1a20d4ed891dbe59e448ce36
James Jordan <jpjordan1987@gmail.com>
2021-02-24 20:58:20 -0500

    Some initial html marking down, and outline of javascript to-dos

---
+++ Assets/javascript/script.js
@@ -0,0 +1,13 @@
+// GIVEN a weather dashboard with form inputs--form input built
+
+
+// WHEN I search for a city// THEN I get current future conditions, that city is added to the search history
+
+    //--ul display of search history built
+    //Working API Call 1 - api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=16d579fb98020ce8daf7d5ea4ad1f4c3
+
+
+// WHEN I view current city weather// THEN I get city name, the date, an icon representation of weather conditions, temperature, humidity, wind speed, and UV index
+// WHEN I view the UV indexTHEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city// THEN a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, humidity
+// WHEN I click on a city in the search history// THEN I am again presented with current and future conditions for that city
