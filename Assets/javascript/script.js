56f3e9348143c0f7be999458b72ec623f6d300fc
CodeWarrior <robertajjordan@Robertas-MacBook-Pro.local>
2021-03-04 22:26:03 -0500

    local storage functions working beginning on fetches

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,56 +1,49 @@
 // WHEN I search for a city// THEN I get current future conditions, that city is added to the search history
+var apiKey='16d579fb98020ce8daf7d5ea4ad1f4c3';
 var cityInpt=$('#city');
 var submitBtn = $('#submit');
-var citiesSearched=[];
-var apiKey='16d579fb98020ce8daf7d5ea4ad1f4c3';
-var cityName='Charlotte';
-var nowWeatherUrl='api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+var searchedCityList=$('#searched-cities');
+var citiesSearched=JSON.parse(localStorage.getItem(('citiesSrchdStore'))) || [];
 
+// load recent Cities to page
+for (item of citiesSearched) {searchedCityList.append(('<li>'+ item + '</li>'))};
 
-//(JSON.parse(localStorage.getItem(citiesSearched)
+//set event listener
 
 submitBtn.click(function(){
     event.preventDefault();
-    var printCity= cityInpt.val();
-    console.log(printCity.toString());
-    citiesSearched.push(printCity);
-    localStorage.setItem('citiesSearched', JSON.stringify(citiesSearched));
-    $("ul").append('<li>'+ printCity+ '</li>');
+    citiesSearched.push(cityInpt.val());
+    if ((citiesSearched.length)> 10) {citiesSearched.splice(0,1)};
+    localStorage.setItem('citiesSrchdStore', JSON.stringify(citiesSearched));
+    $("ul").append('<li>'+ cityInpt.val() + '</li>');
+    getWeather(cityInpt.val());
   });
+
+function getWeather(cityName){
+  var nowWeatherUrl='https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+  fetchProcess(nowWeatherUrl)};
+
+function getForecast(){
+  var nowWeatherUrl='https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+  fetchProcess(nowWeatherUrl)};
+
+
+function fetchProcess(requestUrl){
+  fetch(requestUrl)
+  .then(function (response) {
+    return response.json();})
+  .then(function (data) {
+    console.log(data) //change action to purpose
+    }
+  )};
 
 // function getApi() {
 //     // fetch request gets a list of all the repos for the node.js organization
 //     var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
   
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         console.log(data)
-//         //Loop over the data to generate a table, each table row will have a link to the repo url
-//         for (var i = 0; i < data.length; i++) {
-//           // Creating elements, tablerow, tabledata, and anchor
-//           var createTableRow = document.createElement('tr');
-//           var tableData = document.createElement('td');
-//           var link = document.createElement('a');
-  
-//           // Setting the text of link and the href of the link
-//           link.textContent = data[i].html_url;
-//           link.href = data[i].html_url;
-  
-//           // Appending the link to the tabledata and then appending the tabledata to the tablerow
-//           // The tablerow then gets appended to the tablebody
-//           tableData.appendChild(link);
-//           createTableRow.appendChild(tableData);
-//           tableBody.appendChild(createTableRow);
-//         }
-//       });
-//   }
-
     //--ul display of search history built
-    //Working API Call 1 - api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=16d579fb98020ce8daf7d5ea4ad1f4c3
-    //Working API Call 2 - api.openweathermap.org/data/2.5/forecast?q=Charlotte&appid=16d579fb98020ce8daf7d5ea4ad1f4c3
+    //Working API Call 1 - https://api.openweathermap.org/data/2.5/weather?q=Charlotte&appid=16d579fb98020ce8daf7d5ea4ad1f4c3
+    //Working API Call 2 - https://api.openweathermap.org/data/2.5/forecast?q=Charlotte&appid=16d579fb98020ce8daf7d5ea4ad1f4c3
     //Working API Call 3 - https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=16d579fb98020ce8daf7d5ea4ad1f4c3
 
 // WHEN I view current city weather// THEN I get city name, the date, an icon rep of weather conditions, temperature, humidity, wind speed, UV index
